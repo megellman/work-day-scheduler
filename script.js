8c97bb72f7125eac6f34eaddde264b5de3528163
Megan Ellman <meganlellman@gmail.com>
2023-01-07 13:06:40 -0800

    fixed class changing if else statement to include current time

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
     var currentDay = dayjs().format('MMM DD, YYYY')
     $('#currentDay').append(currentDay);
-    now = Number((dayjs().format('HH')));
+    now = Number((dayjs().format('H')));
     var container = $('.container-lg');
-    var hour;
     for (var i = 9; i < 18; i++) {
-        hour = (dayjs().hour(i).format('H'));
-        var timeBlockDiv = $('<div>').addClass('row time-block future').attr('id', hour);
+        // Creating elements 
+        var time = (dayjs().hour(i).format('H'));
+        var timeBlockDiv = $('<div>').addClass('row time-block future').attr('id', time);
         var textArea = $('<textarea>').addClass('col-8 col-md-10 description');
         var hour = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(dayjs().hour(i).format('hA')).attr('id', i);
         var saveBtn = $('<button>').addClass('btn saveBtn col-2 col-md-1');
@@ -19,18 +17,20 @@ $(function () {
         timeBlockDiv.append(saveBtn);
         saveBtn.append(icon);
         container.append(timeBlockDiv);
-        sampleTime = Number(dayjs().format('H'));
-        if (hour.attr('id') < sampleTime) {
-            timeBlockDiv.removeClass('future present');
-            timeBlockDiv.addClass('past');
-        } else if (hour.attr('id') > sampleTime) {
-            timeBlockDiv.removeClass('past present');
-            timeBlockDiv.addClass('future');
-        } else {
-            timeBlockDiv.removeClass('past future');
-            timeBlockDiv.addClass('present');
-        }
+        // Variable for testing time class change 
+        // sampleTime = Number(dayjs().second(1673129942).format('H'));
+            if (hour.attr('id') < now) {
+                timeBlockDiv.removeClass('future present');
+                timeBlockDiv.addClass('past');
+            } else if (hour.attr('id') > now) {
+                timeBlockDiv.removeClass('past present');
+                timeBlockDiv.addClass('future');
+            } else {
+                timeBlockDiv.removeClass('past future');
+                timeBlockDiv.addClass('present');
+            }
     }
+
 
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
