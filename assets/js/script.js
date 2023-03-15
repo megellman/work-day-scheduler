dff35d4e4e783a39ccd07a48ab7c39242d67ec68
Megan Ellman <meganlellman@gmail.com>
2023-03-15 07:57:08 -0700

    saves and pulls from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ $(function () {
     var container = $('.container-lg');
     for (var i = 9; i < 18; i++) {
         // Creating elements 
+        // get corresponding time block from local storage
         var hourBlock = JSON.parse(localStorage.getItem(`Hour-${i}`));
         var timeLongFormat = (dayjs().hour(i).format('H'));
         var timeBlockDiv = $('<div>').addClass('row time-block future').attr('id', timeLongFormat);
@@ -20,22 +21,26 @@ $(function () {
         saveBtn.append(icon);
         container.append(timeBlockDiv);
         
+        // if the hour is in the past, gray background color
         if (i < now) {
             console.log(i + "<" + now + "" + "past")
             timeBlockDiv.removeClass('future present');
             timeBlockDiv.addClass('past');
+        // if the hour is in the future, green background color
         } else if (i > now) {
             console.log(i + ">" + now + "" + "future")
             timeBlockDiv.removeClass('past present');
             timeBlockDiv.addClass('future');
+        // if the hour is in the present, red background color
         } else {
             console.log("current")
             timeBlockDiv.removeClass('past future');
             timeBlockDiv.addClass('present');
         }
     }
+    // saving the text content of field to local storage along with the 12 hour formatted time
     container.on('click', '.saveBtn', function () {
-        var currentEl = $(this).prev('hourInfoSection').val();
+        var currentEl = $(this).prev().val();
         var hour = (Number($(this).parent()[0].id));
         if (hour > 12) { hour = hour - 12; }
         hour = 'Hour-' + hour;
